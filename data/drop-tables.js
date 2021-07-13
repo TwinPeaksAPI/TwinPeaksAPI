788a65ef3ce83d524fcc4ad3f1df2618e881017c
Tucker <tuckerhoog@tutanota.com>
2021-07-13 14:41:00 -0400

    slight refactor of SQL quaries

--- data/drop-tables.js
+++ data/drop-tables.js
@@ -8,10 +8,11 @@ async function run() {
     await client.query(`
       DROP TABLE IF EXISTS persons CASCADE;
       DROP TABLE IF EXISTS quotes CASCADE;
+      DROP TABLE IF EXISTS quotes_persons_junction CASCADE
     `);
 
     console.log('drop tables complete');
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   } finally {
     client.end();

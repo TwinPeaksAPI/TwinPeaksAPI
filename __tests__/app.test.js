c9f03cc2e6daa2e7ec724043a2c1f1dcbb921de3
Tucker <tuckerhoog@tutanota.com>
2021-07-13 17:21:05 -0400

    add route to get quote from random character

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,9 +4,6 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import { formatName } from '../lib/utils/utils.js';
 
-
-
-
 describe('demo routes', () => {
 
   const agent = request.agent(app);
@@ -16,6 +13,7 @@ describe('demo routes', () => {
   });
 
   it.skip('creates a quote via Post', async () => {
+
     const res = await agent
       .post('/api/lclquotes')
       .send({
@@ -30,9 +28,11 @@ describe('demo routes', () => {
       quoteTextOnly: 'She\'s dead... Wrapped in plastic.',
       personsId: '1'
     });
+
   });
 
   it('finds all quotes via GET', async () => {
+
     const quoteOne = {
       id: '45',
       name: 'Dale Cooper',
@@ -53,24 +53,29 @@ describe('demo routes', () => {
     };
 
     const res = await request(app)
-      .get('/api/quotes');
+      .get('/api/quotes/')
+      .send({ limit: 10 });
 
     expect(res.body).toEqual(expect.arrayContaining([quoteOne, quoteTwo, quoteThree]));
   });
 
   it('gets a quote with id via GET', async () => {
+
     const quote = {
       id: '26',
       name: 'Deputy Hawk',
       quoteText: 'Deputy Hawk: One woman can make you fly like an eagle, another can give you the strength of a lion, but only one in the cycle of life can fill your heart with wonder and the wisdom that you have known a singular joy. I wrote that for my girlfriend.',
       quoteTextOnly: 'One woman can make you fly like an eagle, another can give you the strength of a lion, but only one in the cycle of life can fill your heart with wonder and the wisdom that you have known a singular joy. I wrote that for my girlfriend.',
     };
+
     const res = await request(app).get('/api/quotes/26');
+
     expect(res.body).toEqual(quote);
 
   });
 
   it('gets a quote by persons name via GET', async () => {
+
     const quote = {
       id: '17',
       quoteText: '[Cooper\'s dream, sitting in a chair in the red room. The Man from Another Place twitches uncontrollably with his back to Cooper. Cooper stares at a smiling Laura Palmer.] The Man from Another Place: [Claps hands together, speaking in a strangely paced, distorted voice] Let\'s rock! I\'ve got good news. That gum you like is going to come back in style. [about Laura] She\'s my cousin. But doesn\'t she look almost exactly like Laura Palmer? Dale Cooper: But... it is Laura Palmer. Are you Laura Palmer? Laura Palmer: [speaking in a similarly distorted voice] I feel like I know her, but sometimes my arms bend back. The Man from Another Place: She\'s filled with secrets. Where we\'re from, the birds sing a pretty song, and there\'s always music in the air.',
@@ -96,27 +101,38 @@ describe('demo routes', () => {
       quoteText: 'Laura Palmer: Hello, Agent Cooper. You can go out now. Do you recognize me? Dale Cooper: Are you Laura Palmer? Laura Palmer: I feel like I know her, but sometimes my arms bend back. Dale Cooper: Who are you? Laura Palmer: I am Laura Palmer. Dale Cooper: But Laura Palmer is dead. Laura Palmer: I am dead. Yet I live.',
       quoteTextOnly: 'Hello, Agent Cooper. You can go out now. Do you recognize me? Are you Laura Palmer? I feel like I know her, but sometimes my arms bend back. Who are you? I am Laura Palmer. But Laura Palmer is dead. I am dead. Yet I live.',
     };
+
     const res = await request(app).get('/api/characters/laura palmer');
+
     expect(res.body).toEqual([quote, quoteTwo, quoteThree, quoteFour, quoteFive]);
+
   });
 
   it('can search for quotes by letters in words via GET', async () => {
+
     const quote = {
       id: '26',
       quoteText: 'Deputy Hawk: One woman can make you fly like an eagle, another can give you the strength of a lion, but only one in the cycle of life can fill your heart with wonder and the wisdom that you have known a singular joy. I wrote that for my girlfriend.',
       quoteTextOnly: 'One woman can make you fly like an eagle, another can give you the strength of a lion, but only one in the cycle of life can fill your heart with wonder and the wisdom that you have known a singular joy. I wrote that for my girlfriend.',
       name: 'Deputy Hawk'
     };
+
     const res = await request(app).get('/api/quotes/search/EaGlE');
+
     expect(res.body).toEqual([quote]);
+
   });
 
   it('formats a name to lowercase and then uppercases the first letter', async () => {
+
     const actual = formatName('hELLO');
+
     expect(actual).toEqual('Hello');
 
     const actualTwo = formatName('hELLO RFSKJJS');
+
     expect(actualTwo).toEqual('Hello Rfskjjs');
+
   });
 
   it('grabs a random quote VIA GET', async () => {
@@ -134,7 +150,24 @@ describe('demo routes', () => {
   it('limits number of quotes via GET', async () => {
 
     const res = await request(app).get('/api/quotes/limit/5');
+
     expect(res.body.length).toEqual(5);
+
+  });
+
+  it('gets random quote by character', async () => {
+
+    const res = await request(app).get('/api/characters/random/Log Lady');
+
+    expect(res.body).toMatchObject({
+      id: expect.any(String),
+      quoteText: expect.any(String),
+      quoteTextOnly: expect.any(String)
+    });
+
   });
 
 });
+
+
+//want to get random quote by each toon
